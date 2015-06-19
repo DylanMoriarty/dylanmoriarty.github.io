a90fdfa51d519522e56e4081813dbe07071b7d9c
DylanMoriarty <EatinCake@gmail.com>
2015-06-19 13:34:50 -0500

    Now with Wheat & Soy!

--- maps/AgResource/js/main.js
+++ maps/AgResource/js/main.js
@@ -7,12 +7,12 @@ var croptype = null;
 
 //Leaflet Map Properties
 var map = L.map('map',{
-  maxZoom: 10,
-  minZoom: 7,
+  maxZoom: 8,
+  minZoom: 4,
   zoomControl: false
 }).setView([39.381018,-89.318848],7)
 // Set Bounds to the map by a Top-Left and Bottom-Right coordinates
-// .setMaxBounds([ [48.011975, -100.942383],[30.552800, -68.291016] ])
+.setMaxBounds([ [71.566641, -170.332031],[-4.434044, -53.964844] ])
 ;
 
 //Declare Tile Source
@@ -21,23 +21,18 @@ L.tileLayer(
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
   }).addTo(map);
 
-var corn = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":"1","source":"A","farm":"","photos_no":"1","latitude":"40.825505","longitude":"-87.978425","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-87.978425,40.825505]}},{"type":"Feature","properties":{"id":"2","source":"A","farm":"","photos_no":"1","latitude":"40.561246","longitude":"-88.028661","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.028661,40.561246]}},{"type":"Feature","properties":{"id":"3","source":"A","farm":"","photos_no":"1","latitude":"40.414997","longitude":"-88.064453","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.064453,40.414997]}},{"type":"Feature","properties":{"id":"4","source":"P","farm":"","photos_no":"1","latitude":"40.205254","longitude":"-88.240197","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.240197,40.205254]}},{"type":"Feature","properties":{"id":"5","source":"P","farm":"","photos_no":"1","latitude":"39.846498","longitude":"-88.292335","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.292335,39.846498]}},{"type":"Feature","properties":{"id":"6","source":"A","farm":"","photos_no":"1","latitude":"39.857392","longitude":"-88.500862","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.500862,39.857392]}}]}
+var corn = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":"1","source":"A","farm":"","photos_no":"1","latitude":"40.825505","longitude":"-87.978425","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-87.978425,40.825505]}},{"type":"Feature","properties":{"id":"2","source":"A","farm":"","photos_no":"1","latitude":"40.561246","longitude":"-88.028661","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.028661,40.561246]}},{"type":"Feature","properties":{"id":"3","source":"A","farm":"","photos_no":"1","latitude":"40.414997","longitude":"-88.064453","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.064453,40.414997]}},{"type":"Feature","properties":{"id":"4","source":"A","farm":"","photos_no":"1","latitude":"40.205254","longitude":"-88.240197","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.240197,40.205254]}},{"type":"Feature","properties":{"id":"5","source":"A","farm":"","photos_no":"1","latitude":"39.846498","longitude":"-88.292335","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.292335,39.846498]}},{"type":"Feature","properties":{"id":"6","source":"A","farm":"","photos_no":"1","latitude":"39.857392","longitude":"-88.500862","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.500862,39.857392]}}]}
 
-var soy = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":"1","source":"P","farm":"","photos_no":"1","latitude":"40.825505","longitude":"-87.978425","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-87.978425,40.825505]}},{"type":"Feature","properties":{"id":"2","source":"P","farm":"","photos_no":"1","latitude":"40.561246","longitude":"-88.028661","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.028661,40.561246]}},{"type":"Feature","properties":{"id":"3","source":"P","farm":"","photos_no":"1","latitude":"42.414997","longitude":"-80.064453","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.064453,40.414997]}},{"type":"Feature","properties":{"id":"4","source":"P","farm":"","photos_no":"1","latitude":"40.205254","longitude":"-88.240197","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.240197,40.205254]}},{"type":"Feature","properties":{"id":"5","source":"A","farm":"","photos_no":"1","latitude":"39.846498","longitude":"-88.292335","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.292335,39.846498]}}]}
+var soy = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":"1","source":"P","farm":"","photos_no":"1","latitude":"41.825505","longitude":"-87.978425","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-87.978425,41.825505]}},{"type":"Feature","properties":{"id":"2","source":"A","farm":"","photos_no":"1","latitude":"41.561246","longitude":"-88.028661","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.028661,41.561246]}},{"type":"Feature","properties":{"id":"3","source":"P","farm":"","photos_no":"1","latitude":"41.414997","longitude":"-88.064453","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.064453,41.414997]}},{"type":"Feature","properties":{"id":"4","source":"A","farm":"","photos_no":"1","latitude":"41.205254","longitude":"-88.240197","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.240197,41.205254]}},{"type":"Feature","properties":{"id":"5","source":"P","farm":"","photos_no":"1","latitude":"40.846498","longitude":"-88.292335","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.292335,40.846498]}},{"type":"Feature","properties":{"id":"6","source":"A","farm":"","photos_no":"1","latitude":"40.857392","longitude":"-88.500862","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-88.500862,40.857392]}}]}
+
+var wheat = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":"1","source":"A","farm":"","photos_no":"1","latitude":"41.825505","longitude":"-85.978425","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-85.978425,41.825505]}},{"type":"Feature","properties":{"id":"2","source":"P","farm":"","photos_no":"1","latitude":"41.561246","longitude":"-86.028661","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-86.028661,41.561246]}},{"type":"Feature","properties":{"id":"3","source":"A","farm":"","photos_no":"1","latitude":"41.414997","longitude":"-86.064453","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-86.064453,41.414997]}},{"type":"Feature","properties":{"id":"4","source":"P","farm":"","photos_no":"1","latitude":"41.205254","longitude":"-86.240197","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-86.240197,41.205254]}},{"type":"Feature","properties":{"id":"5","source":"A","farm":"","photos_no":"1","latitude":"40.846498","longitude":"-86.292335","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-86.292335,40.846498]}},{"type":"Feature","properties":{"id":"6","source":"P","farm":"","photos_no":"1","latitude":"40.857392","longitude":"-86.500862","date_1":"06/10/15","date_2":"06/16/15","date_3":"","date_4":""},"geometry":{"type":"Point","coordinates":[-86.500862,40.857392]}}]}
+
 
 //CSV Data in GeoJson form
 var geojsonFeature = corn;
 
 //For Switching between Geojsons...
 d3.select(".trigger1").on("click", function(){
-  var geojsonFeature = soy;
-  $('.leaflet-control-zoom').remove();
-  $('.leaflet-marker-icon').remove();
-  $("#feature_infos").fadeOut("fast");
-  makeJson(geojsonFeature);
-});
-
-d3.select(".trigger2").on("click", function(){
   var geojsonFeature = corn;
   $('.leaflet-control-zoom').remove();
   $('.leaflet-marker-icon').remove();
@@ -45,36 +40,39 @@ d3.select(".trigger2").on("click", function(){
   makeJson(geojsonFeature);
 });
 
-function getJson(simp){  //Removed unneeded arguments here
-    var url = 'file' + simp + '.json';
-    map.removeLayer(geojsonLayer);
-    //geojsonLayer.clearLayers();  I don't believe this needed.
-    $.getJSON(url, function(data){
-        geojsonLayer = L.geoJson(data, {
-            style: defaultStyle,
-            onEachFeature: onEachFeature
-        });
-        geojsonLayer.addTo(map);
-    });
-}
+d3.select(".trigger2").on("click", function(){
+  var geojsonFeature = soy;
+  $('.leaflet-control-zoom').remove();
+  $('.leaflet-marker-icon').remove();
+  $("#feature_infos").fadeOut("fast");
+  makeJson(geojsonFeature);
+});
+
+d3.select(".trigger3").on("click", function(){
+  var geojsonFeature = wheat;
+  $('.leaflet-control-zoom').remove();
+  $('.leaflet-marker-icon').remove();
+  $("#feature_infos").fadeOut("fast");
+  makeJson(geojsonFeature);
+});
 
 window.onload = makeJson(geojsonFeature);
-
 
 function makeJson(geojsonFeature){
   //Load Geojson Data
   L.geoJson(geojsonFeature, {
     pointToLayer: function(feature, latlng){
       //Creating Markers- thissource grabs which source the photo is from, A for AgResource, or P for Public.
-      var thissource = feature.properties.source
-      var thisid = String(feature.properties.source)
+      var thissource = feature.properties.source;
+      var thissourcestring = String(feature.properties.source);
+      var thisid = String(feature.properties.id);
 
       //Setup icon properties. Size is the size of the icon, and Anchor is which pixel will be on the Lat/Lng
       var IconRoot = L.Icon.extend({
         options:{
           iconSize: [32, 32],
           iconAnchor: [16, 16],
-          className: thisid,
+          className: thissourcestring+" "+thisid+"iconids",
         }
       })
 
@@ -95,6 +93,8 @@ function makeJson(geojsonFeature){
             croptype = "corn";
           }else if(geojsonFeature == soy){
             croptype = "soy";
+          }else if(geojsonFeature == wheat){
+            croptype = "wheat";
           };
 
           //Need to re-declare this variable...

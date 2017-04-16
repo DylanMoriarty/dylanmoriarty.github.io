888ab48dd029d7ad6e47d3397b9e545a5b49014e
Dylan <Dylan@developmentseed.org>
2017-04-15 21:03:45 -0400

    Make home head banner change based on time of day

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,47 +1,70 @@
 global.$ = global.jQuery = global.jquery = require('jquery');
 require('splitpic');
 
- 
-// today = new Date();
+function checkHeader() {
+  var bannerImage = document.querySelector(".welcomemat-center")
 
-// console.log(today);
+  if (bannerImage) {
+    var rightNow = new Date().getHours()
+    var newImage = "../assets/graphics/banners/main/"
 
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+    if (rightNow < 9) {
+    bannerImage.src=newImage + "morning.jpg"
+    } else if (rightNow < 12) {
+    bannerImage.src=newImage + "brunch.jpg"
+    } else if (rightNow < 16) {
+    bannerImage.src=newImage + "lunch.jpg"
+    } else if (rightNow < 19) {
+    bannerImage.src=newImage + "latenoon.jpg"
+    } else {
+    bannerImage.src=newImage + "evening.jpg"
     }
-}, 250);
+  }
+}
+
+function hideHeader() {
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header').outerHeight();
+
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+}
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
+  var st = $(this).scrollTop();
+
+  // Make sure they scroll more than delta
+  if(Math.abs(lastScrollTop - st) <= delta)
+      return;
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight){
+      // Scroll Down
+      $('header').removeClass('nav-down').addClass('nav-up');
+  } else {
+      // Scroll Up
+      if(st + $(window).height() < $(document).height()) {
+          $('header').removeClass('nav-up').addClass('nav-down');
+      }
+  }
+
+  lastScrollTop = st;
 }
+
+$(document).ready(function() {
+  if (document.querySelector('#front-content')) {
+    checkHeader()
+    hideHeader()
+  }
+})
